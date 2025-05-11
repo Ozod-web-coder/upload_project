677e5722c42f68be2f0fe6aa513924b29dcb7c5d
Leyshol Mazokirov <mazokirovozodbekoff@gmail.com>
2025-05-11 15:35:15 +0500

    upload_v1

--- minio.py
+++ minio.py
@@ -10,8 +10,8 @@ from fastapi import UploadFile, HTTPException, File
 minio_client = boto3.client(
     's3',
     endpoint_url = "http://localhost:9000",
-    aws_access_key_id = "admin",
-    aws_secret_access_key = "12345678",
+    aws_access_key_id = "minioadmin",
+    aws_secret_access_key = "minioadmin",
     config = Config(signature_version='s3v4'),
     region_name = 'us-east-1'
 )
